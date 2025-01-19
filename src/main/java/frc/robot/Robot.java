bd61a21286d9efe7e8911fdec983bb48e634e342
annaburckart <159260333+annaburckart@users.noreply.github.com>
2025-01-19 16:52:24 -0500

    updated configuration for id for drive and imu

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * the TimedRobot documentation. If you change the na                                                                                                                                 me of this class or the package after creating
  * this project, you must also update the Main.java file in the project.
  */
 public class Robot extends TimedRobot {
